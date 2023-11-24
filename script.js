e604089786a11668d55769ad55988883b8d63b5b
Christian Carlo M. Galarrita <20210023170@my.xu.edu.ph>
2023-11-25 03:47:33 +0800

    login authentication part 2

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function authenticateUser(username, password) {
-  const predefinedUsername = "admin";
-  const predefinedPassword = "password";
+  const mongoose = require('mongoose');
+  const User = require('./models/user'); // Assuming you have a User model
 
-  if (username === predefinedUsername && password === predefinedPassword) {
-    console.log("User authenticated successfully");
-    // Redirect to the user page
-    window.location.href = "userprofile.html";
-    return true;
-  } else {
-    console.log("Authentication failed");
-    return false;
+  async function authenticateUser(username, password) {
+    // Connect to MongoDB
+    await mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
+    // Find user in the database
+    const user = await User.findOne({ username: username });
+
+    if (user && user.password === password) { // In real-world applications, never store passwords in plain text
+      console.log("User authenticated successfully");
+      // Redirect to the user page
+      window.location.href = "userprofile.html";
+      return true;
+    } else {
+      console.log("Authentication failed");
+      return false;
+    }
   }
 }
