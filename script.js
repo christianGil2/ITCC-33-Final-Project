60440bd5149530f7d77b1297624d99567ee84f9c
Christian Carlo M. Galarrita <20210023170@my.xu.edu.ph>
2023-11-25 03:45:11 +0800

    login authentication

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-    // Add a click event listener to the link
-    document.getElementById("page-link").addEventListener("click", function (e) {
-      // Prevent the default link behavior (navigating to the href)
-      e.preventDefault();
-  
-      // Add the class to trigger the transition
-      document.getElementById("page-transition").classList.add("square-animation");
-  
-      // After a delay, navigate to the new page
-      setTimeout(function () {
-        window.location.href = e.target.href;
-      }, 2500); // Adjust the delay to match your animation duration
-    });
-  });
-  
+function authenticateUser(username, password) {
+  const predefinedUsername = "admin";
+  const predefinedPassword = "password";
+
+  if (username === predefinedUsername && password === predefinedPassword) {
+    console.log("User authenticated successfully");
+    // Redirect to the user page
+    window.location.href = "userprofile.html";
+    return true;
+  } else {
+    console.log("Authentication failed");
+    return false;
+  }
+}
